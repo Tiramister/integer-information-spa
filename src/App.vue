66c8ee17eee2767ce3d46916d79362e311e3ea16
Tiramister <nh111424@gmail.com>
2022-07-23 12:31:41 +0000

    feat(App.vue): ヘッダーとタイトルを実装

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,10 @@
-<template></template>
+<template>
+  <v-app>
+    <v-layout>
+      <v-app-bar color="primary" density="comfortable">
+        <v-app-bar-title>Integer Information</v-app-bar-title>
+      </v-app-bar>
+      <v-main></v-main>
+    </v-layout>
+  </v-app>
+</template>
