79e18fb6c3d8f6c7e3c7be4d17afb57e102b7185
Tiramister <nh111424@gmail.com>
2022-07-23 10:26:39 +0000

    feat(vuetify): カラーテーマを変更

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,5 +5,21 @@ import "vuetify/styles";
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: "#0d6efd",
+          secondary: "#009688",
+          accent: "#673ab7",
+          error: "#e91e63",
+          warning: "#ffc107",
+          info: "#cff4fc",
+          success: "#8bc34a",
+        },
+      },
+    },
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
