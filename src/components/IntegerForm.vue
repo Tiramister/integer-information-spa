a67d50a96c4d6b39c7026a0799844badbc84fad0
Tiramister <nh111424@gmail.com>
2022-07-24 06:53:19 +0000

    feat(Integerform): 入力フォームを作成

--- src/components/IntegerForm.vue
+++ src/components/IntegerForm.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+  <v-container>
+    <v-row>
+      <v-form class="w-100">
+        <v-text-field
+          style="width: min(100%, 500px)"
+          v-model="integer"
+          label="1 以上 1e18 以下の整数"
+        ></v-text-field>
+      </v-form>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+let integer: BigInt = BigInt(1);
+</script>
