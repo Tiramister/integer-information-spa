933223b668f87af379d3eb841ae39271ff6bf2eb
Tiramister <nh111424@gmail.com>
2022-07-24 07:09:59 +0000

    feat(IntegerForm): 入力フォームの検証を実装

--- src/components/IntegerForm.vue
+++ src/components/IntegerForm.vue
@@ -1,17 +1,25 @@
 <template>
   <v-container>
     <v-row>
-      <v-form class="w-100">
-        <v-text-field
-          style="width: min(100%, 500px)"
-          v-model="integer"
-          label="1 以上 1e18 以下の整数"
-        ></v-text-field>
-      </v-form>
+      <v-col>
+        <v-form class="w-100">
+          <v-text-field
+            style="width: min(100%, 500px)"
+            v-model="integer"
+            :rules="[validateInteger]"
+            label="1 以上 1e18 以下の整数"
+          ></v-text-field>
+        </v-form>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-let integer: BigInt = BigInt(1);
+import { Ref, ref } from "vue";
+
+let integer: Ref<BigInt> = ref(BigInt(1));
+const validateInteger = (v: any) =>
+  /^([1-9][0-9]{0,17}|10{18})$/.test(v) ||
+  "1 以上 1e18 以下の整数を入力してください";
 </script>
