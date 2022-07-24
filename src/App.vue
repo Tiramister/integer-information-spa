4722f6e22ec6936e647b8604caa38da19f5ed1df
Tiramister <nh111424@gmail.com>
2022-07-24 06:24:41 +0000

    refactor(IntegerForm): IntegerForm コンポーネントを作成

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <v-app>
     <v-layout>
       <Header />
-      <v-main></v-main>
+      <v-main>
+        <IntegerForm />
+      </v-main>
     </v-layout>
   </v-app>
 </template>
 
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import IntegerForm from "./components/IntegerForm.vue";
 </script>
