69073548709126e187bab0ab9d0614f93ed47e58
Tiramister <nh111424@gmail.com>
2022-07-25 13:56:53 +0000

    feat(pow): pow 関数を実装

---
+++ src/components/top/logics/pow.ts
@@ -0,0 +1,19 @@
+function pow(x: bigint, e: bigint, mod: bigint | undefined) {
+  let base = 1n;
+  let result = 1n;
+
+  while (e !== 0n) {
+    if (e % 2n !== 0n) {
+      result *= base;
+      if (mod !== undefined) result %= mod;
+    }
+
+    base *= x;
+    if (mod !== undefined) base %= mod;
+    e /= 2n;
+  }
+
+  return result;
+}
+
+export default pow;
