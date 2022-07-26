89dacdf1ecaee760c9d584e0dd547b9a64cb758f
Tiramister <nh111424@gmail.com>
2022-07-26 22:19:57 +0000

    test(totient): totient テストの縮小

--- src/logics/totient.test.ts
+++ src/logics/totient.test.ts
@@ -1,8 +1,7 @@
 import totient from "./totient";
 import gcd from "./gcd";
-import random from "./random";
 
-export function totientNaive(n: bigint): bigint {
+function totientNaive(n: bigint): bigint {
   let result = 0n;
   for (let m = 1n; m <= n; ++m) {
     if (gcd(n, m) === 1n) ++result;
@@ -14,16 +13,6 @@ test("less than 1,000", () => {
   const upper = 1000n;
 
   for (let n = 1n; n < upper; ++n) {
-    expect(totient(n)).toBe(totientNaive(n));
-  }
-});
-
-test("50 random cases in [100,000, 200,000)", () => {
-  const lower = 100_000n;
-  const upper = 200_000n;
-
-  for (let i = 0; i < 50; ++i) {
-    let n = random(upper - lower) + lower;
     expect(totient(n)).toBe(totientNaive(n));
   }
 });
