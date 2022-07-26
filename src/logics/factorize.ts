5adb920f96c475c3ba36be67cc31ed321fcc9347
Tiramister <nh111424@gmail.com>
2022-07-26 04:45:21 +0000

    feat(factorize): 愚直な素因数分解を実装

---
+++ src/logics/factorize.ts
@@ -0,0 +1,29 @@
+type Factor = {
+  base: bigint;
+  exp: bigint;
+};
+
+function factorize_naive(n: bigint): Factor[] {
+  let result: Factor[] = [];
+  for (let base = 2n; base * base <= n; ++base) {
+    if (n % base !== 0n) continue;
+
+    let exp = 0n;
+    while (n % base === 0n) {
+      n /= base;
+      ++exp;
+    }
+    result.push({ base, exp });
+  }
+  if (n !== 1n) {
+    result.push({ base: n, exp: 1n });
+  }
+
+  result.sort((lhs: Factor, rhs: Factor) =>
+    lhs.base < rhs.base ? -1 : lhs.base > rhs.base ? 1 : 0
+  );
+  return result;
+}
+
+export type { Factor };
+export { factorize_naive };
