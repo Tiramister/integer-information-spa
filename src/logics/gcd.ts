d888c3ec9085b943100bcba928d44562a17b0997
Tiramister <nh111424@gmail.com>
2022-07-26 06:41:33 +0000

    fix(gcd): 引数が負の場合、絶対値を取るように修正

--- src/logics/gcd.ts
+++ src/logics/gcd.ts
@@ -1,4 +1,11 @@
+/**
+ * 2つの整数の最大公約数を求める.
+ * 引数が負の場合、絶対値を取る.
+ */
 function gcd(x: bigint, y: bigint): bigint {
+  if (x < 0n) x = -x;
+  if (y < 0n) y = -y;
+
   if (x < y) [x, y] = [y, x];
 
   while (y !== 0n) [x, y] = [y, x % y];
