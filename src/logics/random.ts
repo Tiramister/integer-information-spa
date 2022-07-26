d6fb12f40e8c462d9d42371ac54958ef355246cb
Tiramister <nh111424@gmail.com>
2022-07-26 06:41:59 +0000

    feat(random): 乱数生成関数を実装

---
+++ src/logics/random.ts
@@ -0,0 +1,18 @@
+/** 100(=50*2)bit 符号なし整数を乱択. */
+function random100(): bigint {
+  const two50 = 1 << 50;
+  const upper = BigInt(Math.floor(Math.random() * two50));
+  const lower = BigInt(Math.floor(Math.random() * two50));
+  return (upper << 50n) + lower;
+}
+
+/**
+ * [0, n)内の整数を乱択.
+ *
+ * @param n - 2^100 より十分小さい整数
+ */
+function random(n: bigint): bigint {
+  return random100() % n;
+}
+
+export default random;
