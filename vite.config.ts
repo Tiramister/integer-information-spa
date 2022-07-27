775e77d95a1afcde2f5987ea528dc78337ee9545
Tiramister <nh111424@gmail.com>
2022-07-27 02:18:08 +0000

    build(vite): target を es2020 に設定

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
+  build: {
+    target: 'es2020'
+  }
 });
