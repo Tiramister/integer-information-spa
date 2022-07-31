e31009b57efbecb04a0f6c8c03bfd6db943e3b02
Tiramister <nh111424@gmail.com>
2022-07-31 10:44:50 +0900

    fix(router): base を正しく /integer/ に設定

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "./components/MainPage/MainPage.vue";
 
 export default createRouter({
-  history: createWebHistory(),
-  routes: [{ path: "/integer/", name: "MainPage", component: MainPage }],
+  history: createWebHistory("/integer/"),
+  routes: [
+    { path: "/", name: "MainPage", component: MainPage },
+  ],
 });
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
-  base: "./",
+  base: "/integer/",
   build: {
     target: 'es2020'
   }
