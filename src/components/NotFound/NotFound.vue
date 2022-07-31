9f645856d1905574856cb3e246815efe3489ab50
Tiramister <nh111424@gmail.com>
2022-07-31 10:45:30 +0900

    feat(NotFound): 404 用のページを作成

---
+++ src/components/NotFound/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-alert type="error">Not Found</v-alert>
+  <MainPageResult integer="404" />
+</template>
+
+<script setup lang="ts">
+import MainPageResult from "../MainPage/MainPageResult.vue";
+</script>
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "./components/MainPage/MainPage.vue";
+import NotFound from "./components/NotFound/NotFound.vue";
 
 export default createRouter({
   history: createWebHistory("/integer/"),
   routes: [
     { path: "/", name: "MainPage", component: MainPage },
+    { path: "/:catchAll(.*)", name: "NotFound", component: NotFound }
   ],
 });
